e377b27c0b333bca24798776e7ccc8792b94d938
Ryan Resella <ryanresella@gmail.com>
2012-07-21 23:34:35 -0500

    Add relations on teams

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,7 @@ class Schedule < ActiveRecord::Base
   attr_accessible :away_team_id, :home_team_id, :week_id, :start_time
 
   belongs_to :week
+  belongs_to :away_team, :class_name => "Team", :foreign_key => "away_team_id"
+  belongs_to :home_team, :class_name => "Team", :foreign_key => "home_team_id"
+
 end
